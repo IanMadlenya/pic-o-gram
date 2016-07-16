ea792860d9fd4c96fb5dd71ebe695831458f87ba
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-16 23:58:44 +0100

    made pics clickable

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-  has_attached_file :image, style:  { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  has_attached_file :image, style:  { large: "600x600>", medium: "300x300>", thumb: "150x150#"},
+  default_url: "images/000/000/003/original/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
