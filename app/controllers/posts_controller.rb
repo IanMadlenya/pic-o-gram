685354ebdc9f9aa6b7feffd33ebae73ecfed5144
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-17 12:28:59 +0100

    DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
   def new
     @post = Post.new
-
   end
 
   def create
@@ -15,21 +14,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find( params[:id] )
   end
 
   def edit
-    @post = Post.find( params[:id] )
   end
 
   def update
-    @post = Post.find( params[:id] )
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      redirect_to(post_path(@post))
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -37,5 +35,9 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
